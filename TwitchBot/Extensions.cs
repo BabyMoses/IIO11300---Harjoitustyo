3cdb2030cbf6ea4223e631f7ec544bfdc47fe28f
BabyMoses <topi.rytkonen@gmail.com>
2016-04-12 16:26:12 +0300

    GUI-improvements, added error handling, tidying up

---
+++ TwitchBot/Extensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace TwitchBot
+{
+    public static class Extensions
+    {
+        public static void AppendText(this RichTextBox box, string text, string color)   //lisärimethodi appendiin värien vaihtamisille (napattu suoraan netistä)
+        {
+            BrushConverter bc = new BrushConverter();
+            TextRange tr = new TextRange(box.Document.ContentEnd, box.Document.ContentEnd);
+            tr.Text = text;
+            try
+            {
+                tr.ApplyPropertyValue(TextElement.ForegroundProperty,
+                    bc.ConvertFromString(color));
+            }
+            catch (FormatException) { }
+        }
+    }
+}
--- TwitchBot/MainWindow.xaml.cs
+++ TwitchBot/MainWindow.xaml.cs
@@ -6,6 +6,8 @@ using System.IO;
 using System.Net;
 using System.Net.Sockets;  //tarvitaan TCP-yhteyteen
 using System.Windows;
+using System.Windows.Media;
+
 
 namespace TwitchBot
 {
@@ -17,12 +19,8 @@ namespace TwitchBot
     public partial class MainWindow : Window
     {
 
-
-        WebClient m_WebClient;  //apia
-        string apiUrl;          //apia
-
-
-        String oauth = ConfigurationManager.AppSettings["Path"];  //oauth polku configista
+        WebClient m_WebClient;
+        string defaultApiUrl;
         TcpClient tcpClient;
         StreamReader reader;
         StreamWriter writer;
@@ -30,169 +28,163 @@ namespace TwitchBot
         string password;
         string channelName;
         string chatMessagePrefix;
-        string chatCommandId;
+        string chatPrefix;
+        string oauth;
         DateTime lastMessage;
         Queue<string> sendMessageQueue;
-        Boolean timer_Running = false;
+        Boolean botRunning;
 
 
         public MainWindow()
         {
 
+            botRunning = false;
             m_WebClient = new WebClient();
-            this.apiUrl = "https://api.twitch.tv/kraken/streams/";
-
-
+            this.defaultApiUrl = "https://api.twitch.tv/kraken/streams/";  //streamien JSON urlin sijainti
             sendMessageQueue = new Queue<string>(); //viesteille jono
             this.userName = ConfigurationManager.AppSettings["Username"];  //twitch käyttäjän nimi LOWER CASE
-            this.channelName = ConfigurationManager.AppSettings["Channel"]; //chattihuone (ROOM) johon liitytään
-            this.password = File.ReadAllText(oauth); //twitch chat OAuth Avain
-            //this.channelName = userName;
-            chatCommandId = "PRIVMSG";
-            chatMessagePrefix = ":" + userName + "!" + userName + "@" + userName + ".tmi.twitch.tv PRIVMSG #" + channelName + " :";  //viestit näyttää chattiin menneessää tältä
+            this.channelName = ConfigurationManager.AppSettings["Channel"]; //chattihuone (ROOM) johon liitytään       
+            this.oauth = ConfigurationManager.AppSettings["Path"];  //oauth polku configista
+            chatPrefix = "PRIVMSG";
+            chatMessagePrefix = ":" + userName + "!" + userName + "@" + userName + ".tmi.twitch.tv PRIVMSG #" + channelName + " :";  //viestit lähtee chattiin tässä muodossa
 
             InitializeComponent();
-            Timer();
-          
+
+            if (File.Exists(oauth))
+            {
+                this.password = File.ReadAllText(oauth); //twitch chat OAuth Avaimen polku appconffista
+            }
+            else
+            {
+                txtChat.AppendText("\r\n" + "The Filepath: " + oauth + " for the Oauth-key seems to be wrong" + "\r\n", "red");
+            }
 
 
-            /*timeri chatin päivitykselle + botti
-            System.Windows.Threading.DispatcherTimer dispatcherTimer2 = new System.Windows.Threading.DispatcherTimer();
-            dispatcherTimer2.Tick += new EventHandler(dispatcherTimer_Tick);
-            dispatcherTimer2.Interval = new TimeSpan(0, 0, 0, 0, 500);  //päivittää joka sekunti
-             * */
+            try
+            {
+                Reconnect();
+            }
+            catch (Exception)
+            {
+                txtChat.AppendText("Error connecting to the chat ", "Red");
+            }
+
+        }
+
+        private void Reconnect() //chatpannulle/chatroomiin liittyminen
+        {
+
+            tcpClient = new TcpClient("irc.twitch.tv", 6667);  //tcp-yhteyden luonti
+            reader = new StreamReader(tcpClient.GetStream());
+            writer = new StreamWriter(tcpClient.GetStream());
+            writer.WriteLine("PASS " + password + Environment.NewLine + "NICK " + userName + Environment.NewLine);  //tällä autentikoidaan ihtemme pannulle
+            //writer.WriteLine("CAP REQ :twitch.tv/membership"); //tulostaa chatin moderaattorit (turha)
+            writer.WriteLine("JOIN #" + channelName);  //chatroomiin liittyminen
+            writer.Flush();
+            lastMessage = DateTime.Now;  //timestamppi tulille
+
+
+            //katsotaan menikö yhdistys läpi
+            var message = reader.ReadLine();
+            if (message.Contains("001"))
+            {
+                txtChat.AppendText("SUCCESFULLY CONNECTED TO CHATROOM #" + channelName + "..." + "\r\n", "Chartreuse");
+                Timer(); // TIMERI PÄÄLLE
+            }
+
+            else
+            {
+                txtChat.AppendText("\r\n" + "ERROR WHILE LOGGING IN TO CHAT, PLEASE CHECK YOUR CREDENTIALS IN .CFG AND START THE PROGRAM AGAIN" + "\r\n", "Red");
+            }
+
+
         }
 
         private void Timer()
         {
             System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
             dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
-            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 500);  //päivittää joka sekunti
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 250);  //päivittää joka 1/4 sekunti
             dispatcherTimer.Start();
-            Reconnect();
-
-            /*
-
-            if (timer_Running == true)
-            {
-                //timeri chatin päivitykselle
-                
-                
-            }
-            else
-            {
-
-                dispatcherTimer.Stop();
-                txtChat.AppendText("\r\n" + "BOOLEAN ON FALSE NYT");
-            }
-            */
-
         }
 
-
-
-
-
-        private void Reconnect()
+        private void HandleChecked(object sender, RoutedEventArgs e)  // BOTTI PÄÄLLÄ (ON)
         {
-            tcpClient = new TcpClient("irc.twitch.tv", 6667);  //tcp-yhteyden luonti
-            reader = new StreamReader(tcpClient.GetStream());
-            writer = new StreamWriter(tcpClient.GetStream());
-
-
-
-            writer.WriteLine("PASS " + password + Environment.NewLine
-                + "NICK " + userName + Environment.NewLine
-                + "USER " + userName + " 8 * :" + userName);  //tällä autentikoidaan ihtemme pannulle
-            writer.WriteLine("CAP REQ :twitch.tv/membership"); //tulostaa chatin käyttäjät
-            writer.WriteLine("JOIN #" + channelName);  //chatroomiin liittyminen
-
-
-            writer.Flush();
-            lastMessage = DateTime.Now;
-
-            txtChat.AppendText("\r\n"+"SUCCESFULLY CONNECTED TO CHATROOM #"+channelName); //tulostus
-
-
-        }
-
-
-        /*
-
-        private void HandleChecked(object sender, RoutedEventArgs e)
-        {
-            txtChat.AppendText("\r\n" + "toggled XD");
-            timer_Running = true;
+            txtChat.AppendText("\r\n" + "BOT IS NOW UP AND RUNNING!!" + "\r\n", "Chartreuse");
+            botRunning = true;
+            sendMessageQueue.Clear();
             Timer();
-
         }
 
-        private void HandleUnchecked(object sender, RoutedEventArgs e)
+        private void HandleUnchecked(object sender, RoutedEventArgs e)  // BOTTI POIS (OFF)
         {
-            txtChat.AppendText("\r\n" + "Untoggled XD");
-            timer_Running = false;
-            Timer();
-         
-
+            txtChat.AppendText("\r\n" + "BOT IS NOW OFFLINE =(" + "\r\n", "red");
+            sendMessageQueue.Clear();
+            botRunning = false;
         }
 
-        */
-        void dispatcherTimer_Tick(object sender, EventArgs e) //homma toimii timerin perässä
-        {
 
+        void dispatcherTimer_Tick(object sender, EventArgs e) //homma toimii timerin tickien perässä
+        {
 
             if (!tcpClient.Connected)
             {
-                
                 Reconnect();
-                txtChat.AppendText("\r\n" + "SUCCESFULLY CONNECTED TO CHATROOM #" + channelName); //tulostus
+            }
+
+            try
+            {
+                if (botRunning == true)
+                {
+                    TrySendingMessages();
+                }
+
+                TryReceiveMessages();
+
+            }
+
+            catch (Exception ex)
+            {
+                txtChat.AppendText("Error getting/sending messages: " + ex.ToString() + "\r\n", "Red");
             }
 
 
-            TrySendingMessages();
-            TryReceiveMessages();
-            loadApistats();
+            try
+            {
+                loadApistats();
+            }
+            catch (Exception ex)
+            {
+                txtChat.AppendText("Error loading Twitch API: " + ex.ToString() + "\r\n", "Red");
 
-            //txtChat.AppendText("\r\n" + DateTime.Now.Second.ToString());  //testi
+            }
 
-            // var massage = reader.ReadLine();
-            // txtChat.AppendText("\r\n" + massage);
             txtChat.ScrollToEnd();
-            
-
 
         }
 
-        
-        void loadApistats()  //apia
+
+        void loadApistats()  //viewers, followers, total views tiedot apista
         {
-            //var url = apiUrl + userName;  //käytä tätä
-           //var url = "https://api.twitch.tv/kraken/streams/steel_tv";
 
-           apiUrl = "https://api.twitch.tv/kraken/streams/"+channelName;
-           string json = m_WebClient.DownloadString(apiUrl);
+            String apiUrl = defaultApiUrl + channelName;
+            string json = m_WebClient.DownloadString(apiUrl);
+            TwitchTV twitchTV = JsonConvert.DeserializeObject<TwitchTV>(json);  //newtonsoftin json desiarialiser tekee jsonista parsettamisen todella helpoksi
 
-          TwitchTV twitchTV = JsonConvert.DeserializeObject<TwitchTV>(json);
-
-          if (twitchTV.stream != null)
-          {
-
-              // if (twitchTV.stream.channel.followers != 0L) 
-              lblViewers.Content = string.Format("Viewers {0:000}", twitchTV.stream.viewers);
-              lblFollowers.Content = string.Format("Followers {0:000}", twitchTV.stream.channel.followers);
-              lblViews.Content = string.Format("Views {0:000}", twitchTV.stream.channel.views);
-          }
-          else
-          {
-              lblViewers.Content = "Stream Offline";
-              lblFollowers.Content = "";
-              lblViews.Content = "";
-          }
-
+            if (twitchTV.stream != null)  //apin json statsit saatavilla *VAIN* jos streami on online
+            {
+                lblViewers.Content = string.Format("Viewers {0:000}", twitchTV.stream.viewers);
+                lblFollowers.Content = string.Format("Followers {0:000}", twitchTV.stream.channel.followers);
+                lblViews.Content = string.Format("Views {0:000}", twitchTV.stream.channel.views);
+            }
+            else
+            {
+                lblViewers.Foreground = new SolidColorBrush(Colors.Red);
+                lblViewers.Content = "Stream Offline";
+                lblFollowers.Content = "";
+                lblViews.Content = "";
+            }
         }
-        
-
-
 
 
         void TrySendingMessages()
@@ -215,8 +207,6 @@ namespace TwitchBot
             if (tcpClient.Available > 0 || reader.Peek() >= 0)
             {
 
-                
-
                 var message = reader.ReadLine();
                 var iCollon = message.IndexOf(":", 1);
 
@@ -231,22 +221,19 @@ namespace TwitchBot
                 if (iCollon > 0)
                 {
                     var command = message.Substring(1, iCollon + 1); //viesti tulee kahden kaksoispisteen välissä-> kaapataan se sieltä (skipataan eka ":")
-                    if (command.Contains(chatCommandId))  //pelkästään chattiin lähetetyt viestit (ei irc:n muuta roskaa) suodatetaan PRIVMSG:n avulla
+                    if (command.Contains(chatPrefix))  //pelkästään chattiin lähetetyt viestit (ei irc:n muuta roskaa) suodatetaan PRIVMSG:n avulla
                     {
                         var iExl = command.IndexOf("!"); //käyttäjän nimestä koppi huutomerkistä
                         var speaker = command.Substring(0, iExl);
                         var chatMessage = message.Substring(iCollon + 1);
 
-                        ReceiveMessage(speaker, chatMessage);
+                        ReceiveMessage(speaker, chatMessage);  //heitetään parsitut käyttäjät/viestit "tulostimeen" 
 
                     }
 
                 }
 
-              // txtChat.AppendText("\r\n" + message);  //kaikki mitä ircci syöttää
-
-
-
+                //txtChat.AppendText("\r\n" + message);  //kaikki mitä ircci syöttää, jos tarvii debugailla
 
             }
         }
@@ -255,20 +242,18 @@ namespace TwitchBot
         {
 
 
-            txtChat.AppendText("\r\n" + speaker + ": " + message); //tulostus
-            //txtChat.AppendText(reader.ReadLine());
+            txtChat.AppendText("\r\n" + speaker + ": ", "DarkOrange"); //käyttäjän tulostus 
+            txtChat.AppendText(message, "#FFCDCDCD");  //viestin tulostus
 
-
-   
             //commands
             if (message.StartsWith("!commands"))
             {
-                SendMessage("Current commands include !moro, !toucan, !merio, !marjo, !aim, !specs");
+                SendMessage("Current commands include !moro, !toucan, !merio, !marjo, !specs");
             }
 
             if (message.StartsWith("!moro"))
             {
-                SendMessage("Mene roskiin " + speaker);
+                SendMessage("No morjesta morjesta " + speaker + "!");
             }
 
 
@@ -277,7 +262,6 @@ namespace TwitchBot
                 SendMessage("                 ▄▄▄▀▀▀▄▄███▄ ░░░░░▄▀▀░░░░░░░▐░▀██▌ ░░░▄▀░░░░▄▄███░▌▀▀░▀█ ░░▄█░░▄▀▀▒▒▒▒▒▄▐░░░░█▌ ░▐█▀▄▀▄▄▄▄▀▀▀▀▌░░░░░▐█▄ ░▌▄▄▀▀░░░░░░░░▌░░░░▄███████▄ ░░░░░░░░░░░░░▐░░░░▐███████████▄ ░░░░░le░░░░░░░▐░░░░▐█████████████▄ ░░░░toucan░░░░░░▀▄░░░▐██████████████▄ ░░░░░░has░░░░░░░░▀▄▄████████████████▄ ░░░░░arrived░░░░░░░░░░░░█▀██████");
             }
 
-         
 
             if (message.StartsWith("!merio"))
             {
@@ -311,18 +295,18 @@ namespace TwitchBot
                 SendMessage("Nikolas on pyllynaama");
             }
 
-            //bännit
-            if (message.Contains("slaidu"))
+            if (message.Contains("ohjelmoin"))
             {
-                SendMessage("/timeout "+speaker+" 30");
+                SendMessage("Ohjelmointi on todella kivaa ja palkitsevaa puuhaa! KappaRoss");
+            }
+
+            //bännit
+            if (message.Contains("slaidu") || message.Contains("lompsa"))
+            {
+                SendMessage("/timeout " + speaker + " 30");
                 SendMessage("Erittäin loso sana");
             }
 
-            if (message.Contains("lompsa"))
-            {
-                SendMessage("/timeout " + speaker + " 60");
-                SendMessage("Erittäin loso sana");
-            }
         }
 
 
@@ -330,11 +314,7 @@ namespace TwitchBot
         void SendMessage(string message)
         {
             sendMessageQueue.Enqueue(message);  //viesti jonoon
-
-
         }
-
-
 
 
     }
